27670bc3bcb32398b98d44ff112f0ad27db4eddd
dyannejohnathon <dyannejohnathon@gmail.com>
2025-04-22 14:41:39 +0700

    Update gitignore and next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
+  basePath: isProd ? '/demo-2048' : '',
+
   output: "export",
-  reactStrictMode: true,
+  // reactStrictMode: true,
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
+
 };
 
 module.exports = nextConfig;
